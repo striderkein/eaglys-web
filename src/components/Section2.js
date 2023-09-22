f4cd92ad13f96975550dfcb8761956616350010b
shirow ozawa <striderkein@users.noreply.github.com>
2023-09-23 00:14:59 +0900

    refactor:change return value to query from ast

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,24 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-function Section2({ ast }) {
-  const adjustHeight = () => {
-    const textarea = document.querySelector('.ast-area');
-    if (textarea) {
-      textarea.style.height = "inherit";
-      textarea.style.height = (textarea.scrollHeight) + "px";
-    }
-  }
-
-  useEffect(() => {
-    adjustHeight();
-  }, [ast]);
-
+function Section2({ sql }) {
   return (
     <section className="section-2">
       <textarea
-        value={ast}
-        className="ast-area"
-        onChange={adjustHeight}
+        value={sql}
+        className="sql-area"
       />
     </section>
   );
