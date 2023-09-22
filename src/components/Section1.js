89597b65124c8939f9cec377d495a3a9226248d9
shirow ozawa <striderkein@users.noreply.github.com>
2023-09-23 01:32:50 +0900

    feat:enable select statement from selectbox

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Section1({ sql, handleSqlChange, handleTransformChange }) {
+  const [selectedSqlType, setSelectedSqlType] = useState('SELECT');
+
+  const handleSqlTypeChange = event => {
+    const sqlType = event.target.value;
+    let initialSql = '';
+
+    switch (sqlType) {
+      case 'SELECT':
+        initialSql = 'SELECT * FROM my_table';
+        break;
+      case 'INSERT':
+        initialSql = 'INSERT INTO my_table (column1, column2) VALUES (value1, value2)';
+        break;
+      case 'UPDATE':
+        initialSql = 'UPDATE my_table SET column1 = value1 WHERE condition';
+        break;
+      case 'DELETE':
+        initialSql = 'DELETE FROM my_table WHERE condition';
+        break;
+      default:
+        break;
+    }
+
+    setSelectedSqlType(sqlType);
+    handleSqlChange({ target: { value: initialSql } });
+  };
+
   return (
     <section className="section-1">
       <h2>SQL</h2>
+      <select value={selectedSqlType} onChange={handleSqlTypeChange}>
+        <option value="SELECT">SELECT</option>
+        <option value="INSERT">INSERT</option>
+        <option value="UPDATE">UPDATE</option>
+        <option value="DELETE">DELETE</option>
+      </select>
       <textarea
         className="hoge"
         value={sql}
