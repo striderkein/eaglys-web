254fb6323ef6893295b1ce0fe5b4c157ed683c7c
shirow ozawa <striderkein@users.noreply.github.com>
2023-09-23 00:14:59 +0900

    refactor:change return value to query from ast

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import Section2 from './Section2';
 
 function Main() {
   const [sql, setSql] = useState('');
-  const [ast, setAst] = useState('');
+  const [modifiedSql, setModifiedSql] = useState('');
 
   const apiUrl = `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`;
 
@@ -16,10 +16,15 @@ function Main() {
 
   const handleTransformChange = async () => {
     try {
-      const response = await axios.post(`${apiUrl}/parse-sql`, { sql });
-      setAst(JSON.stringify(response.data, null, 2));
+      const ast = await axios.post(`${apiUrl}/parse-sql`, { sql })
+      console.log("API-1 Response:", ast);
+      const modifiedAst = await axios.post(`${apiUrl}/modify-ast`, ast.data );
+      console.log("API-2 Response:", modifiedAst);
+      const rebuiltQuery = await axios.post(`${apiUrl}/rebuild-sql`, modifiedAst.data );
+      console.log("API-3 Response:", rebuiltQuery);
+      setModifiedSql(JSON.stringify(rebuiltQuery.data.query, null, 2).replace(/^"|"$|`/g, ''));
     } catch (error) {
-      console.error("API-1 Error:", error);
+      console.error("API Error:", error);
     }
   };
 
@@ -31,7 +36,8 @@ function Main() {
         handleSqlChange={handleSqlChange}
         handleTransformChange={handleTransformChange}
       />
-      <Section2 ast={ast} />
+      <Section2 sql={modifiedSql} />
+      {/* TODO: Add a section to display the map */}
     </div>
   );
 }
