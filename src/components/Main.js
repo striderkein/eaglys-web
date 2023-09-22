2cd671298b85a97744aa1b1c0f6d28e16e34f928
shirow ozawa <striderkein@users.noreply.github.com>
2023-09-22 15:08:42 +0900

    refactor:関数をApp.jsから切り出す

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import Section1 from './Section1';
 import Section2 from './Section2';
 
-function Main({ sql, handleSqlChange, handleTransformChange, modifiedSql, adjustHeight }) {
+function Main() {
+  const [sql, setSql] = useState('');
+  const [modifiedSql, setModifiedSql] = useState('');
+
+  const apiUrl = `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`;
+
+  const handleSqlChange = (e) => {
+    setSql(e.target.value);
+  };
+
+  const handleTransformChange = async () => {
+    try {
+      const response = await axios.post(`${apiUrl}/parse-sql`, { sql });
+      setModifiedSql(JSON.stringify(response.data, null, 2));
+    } catch (error) {
+      console.error("API-1 Error:", error);
+    }
+  };
+
   return (
     <div className="App">
       <h1>SQL Transformer</h1>
-      <Section1 sql={sql} handleSqlChange={handleSqlChange} handleTransformChange={handleTransformChange} />
-      <Section2 modifiedSql={modifiedSql} adjustHeight={adjustHeight} />
+      <Section1
+        sql={sql}
+        handleSqlChange={handleSqlChange}
+        handleTransformChange={handleTransformChange}
+      />
+      <Section2 modifiedSql={modifiedSql} />
     </div>
   );
 }
